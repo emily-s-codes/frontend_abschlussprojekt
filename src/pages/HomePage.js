38fd94011a5f547c7d6e8ff91526dde19e40ff78
AndiS82 <19schneiderandreas82@gmail.com>
2022-12-12 11:40:47 +0100

    fetch implemented

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Bonus from '../components/Bonus'
 import Navbar from "../components/Navbar"
 import SingleProduct from "../components/SingleProduct"
 import Footer from "../components/Footer"
 
 function HomePage() {
+    const [arrayData, setArrayData] = useState([]);
+
+    useEffect(() => {
+        fetch(`https://dummyjson.com/products`)
+            .then((response) => response.json())
+            .then((arrayData) => {
+                setArrayData(arrayData)
+                console.log(arrayData.products)
+            })
+    }, [])
+
     return (
         <div>
             <Navbar />
