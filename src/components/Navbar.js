50c05b826784714d499a34ea98f7bc2ede96e162
AndiS82 <19schneiderandreas82@gmail.com>
2022-12-12 12:20:59 +0100

    navbar implemented

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import HomePage from '../pages/HomePage';
+import filterButton from "../assets/img/Vector.svg"
+import * as FaIcons from "react-icons/fa";
 
-function Navbar() {
+
+function Navbar(props) {
+    const [usableData, setUsableData] = useState([]);
+
+    // useEffect(() => {
+    //     if (arrayData === undefined) {
+    //         return
+    //     } else if (props.searchTerm) {
+
+    //     } else { }
+    // })
+
+
+
     return (
-        <div>Navbar</div>
+        <div>
+            <input type="text" placeholder={<img src={FaIcons.FaSearch} />}></input>
+            <button><img src={filterButton}></img></button>
+        </div>
     )
 }
 
