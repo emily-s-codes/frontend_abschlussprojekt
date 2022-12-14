354f0ca2c52c2531e88a72f5a61466ff6464801b
AndiS82 <19schneiderandreas82@gmail.com>
2022-12-14 17:33:40 +0100

    splash and onboarding redirects implemented

--- src/pages/SplashScreen.js
+++ src/pages/SplashScreen.js
@@ -1,12 +1,20 @@
 import React from 'react'
+import Icon from "../assets/img/E-Shop_Icon.svg"
+import ShopVector from "../assets/img/E-Shop_Vector.svg"
+import { Link } from "react-router-dom";
 
 
 function SplashScreen() {
     {
         return (
-            <div>
-                <h4>Redirect required</h4>
-            </div>
+            <Link to={"/onboarding"}>
+                <div>
+                    <img alt="" src={Icon}></img><br />
+                    <img alt="" src={ShopVector}></img>
+                    <p>Your Shopping Solution</p>
+                    <p>Click on the Screen to proceed to the Onboarding page🥳</p>
+                </div>
+            </Link>
         )
     }
 }
