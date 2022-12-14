7bc56cd3072bce0c12b4f0551245e0f9511a27b3
emilysupercode <sheil.emily.sc@gmail.com>
2022-12-14 22:17:51 +0100

    styling anpassen

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -3,6 +3,8 @@ import Filter from '../components/Filter';
 import FilteredProductsList from '../components/FilteredProductsList'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
+import { Link } from 'react-router-dom';
+import * as MdIcons from "react-icons/md"
 // PARENT ELEMENT
 function ProductList() {
     const [prod, setProd] = useState([]);
@@ -39,7 +41,9 @@ function ProductList() {
 
 
     return (
-        <div>
+        <div className='productListStyle'>
+            <Link className='filterNav' to={"/home"}><MdIcons.MdOutlineKeyboardArrowLeft /></Link>
+            <h2 className='filterHeadline'>Filters</h2>
             <Navbar />
             <Filter products={prod} setprod={setProd} categories={cat} setcat={setCat} filter={filt} setFilter={setFilt} showFilter={showFilt} setShowFilter={setShowFilt} />
             <FilteredProductsList products={prod} setprod={setProd} categories={cat} setcat={setCat} filter={filt} setFilter={setFilt} showFilter={showFilt} setShowFilter={setShowFilt} filteredProducts={filtProd} setFilteredProducts={setFiltData} />
