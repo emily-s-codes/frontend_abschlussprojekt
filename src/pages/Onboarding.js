3434ccae2ca32f83d737c6fc12450e3fbdfa41e4
AndiS82 <19schneiderandreas82@gmail.com>
2022-12-14 17:01:52 +0100

    splash page implementation started

--- src/pages/Onboarding.js
+++ src/pages/Onboarding.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import Illustration from "../assets/img/Illustration.png"
+import { Link } from "react-router-dom"
 
 function Onboarding() {
+
     return (
-        <div>Onboarding</div>
+        <div>
+            <img src={Illustration}></img>
+            <div>
+                <h2>Biggest Sell at your Fingertips</h2>
+                <p>Find your best products from popular shops without delay</p>
+                <Link to={"/home"}><button className='getStartedButton'>Get Started</button></Link>
+            </div>
+        </div>
     )
+
 }
 
 export default Onboarding
