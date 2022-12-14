63a7ec43231b8050aefc19549593f18f568c6671
jAs0n1337 <steffen.dietrich86@gmail.com>
2022-12-14 15:58:56 +0100

    bugfix

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import * as CiIcons from "react-icons/ci";
-import { Link } from "react-router-dom"
 import { Link } from "react-router-dom";
 
 function SearchBar() {
